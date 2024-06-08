a29a3273f1a5296e4adf16bb131d2a9a59c139aa
Wrdn28 <ifloraxd@gmail.com>
2024-06-08 17:16:40 +0000

    OrionLabs: Rebranding About page
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- src/com/crdroid/settings/fragments/About.java
+++ src/com/crdroid/settings/fragments/About.java
@@ -78,11 +78,11 @@ public class About extends SettingsPreferenceFragment {
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         if (preference == mDonate) {
-            launchUrl("https://crdroid.net/donate.php");
+            launchUrl("https://t.me/OrionOS_prjkt");
         } else if (preference == mSourceUrl) {
-            launchUrl("https://github.com/crdroidandroid");
+            launchUrl("https://github.com/OrionOS-prjkt");
         } else if (preference == mTelegramUrl) {
-            launchUrl("https://t.me/crDroidAndroid");
+            launchUrl("https://t.me/OrionOS_prjkt");
         } else if (preference == mShare) {
             Intent intent = new Intent();
             intent.setAction(Intent.ACTION_SEND);
@@ -93,13 +93,13 @@ public class About extends SettingsPreferenceFragment {
         } else if (preference == mTranslate) {
             launchUrl("https://crdroid.net/translations.php");
         } else if (preference == mWebsite) {
-            launchUrl("https://crdroid.net");
+            launchUrl("https://orionos-prjkt.vercel.app/");
         } else if (preference == mTelegramChannelUrl) {
-            launchUrl("https://t.me/crDroidUpdates");
+            launchUrl("https://t.me/OrionOS_Update");
         } else if (preference == mSponsor) {
-            launchUrl("https://www.scopehosts.com");
+            launchUrl("https://orionos-prjkt.vercel.app/");
         } else if (preference == mBuildServerSponsor){
-            launchUrl("https://www.interserver.net/r/836686");
+            launchUrl("https://orionos-prjkt.vercel.app/");
         }
 
         return super.onPreferenceTreeClick(preference);
