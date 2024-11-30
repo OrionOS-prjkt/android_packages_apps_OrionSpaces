4f89cc68761721b42146e9e6013d271bd75b4e30
Drenzzz <116653610+drenzzz@users.noreply.github.com>
2024-11-30 18:01:34 +0700

    OrionSpaces: Hide ADB and developer setting enable status [2/2]  (#12)
    * OrionSpaces: Introduce boot animation styles [3/3] (#11)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Co-authored-by: Joey Huab <jhuab@edukasyon.ph>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: 0xsharkboy <achille@0xsharkboy.dev>
    
    * OrionSpaces: Hide ADB and developer setting enable status [2/2]
    
    * Code is adapted from cutout force full screen
    * Give authorship
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Tony Mantler <nicoya@google.com>
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    
    ---------
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Co-authored-by: Joey Huab <jhuab@edukasyon.ph>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: 0xsharkboy <achille@0xsharkboy.dev>
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Tony Mantler <nicoya@google.com>
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/crdroid/settings/fragments/HideDeveloperStatusSettings.kt
+++ src/com/crdroid/settings/fragments/HideDeveloperStatusSettings.kt
@@ -44,7 +44,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-import com.android.internal.util.custom.HideDeveloperStatusUtils
+import com.android.internal.util.crdroid.HideDeveloperStatusUtils
 
 import com.android.settings.R
 
@@ -60,7 +60,7 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
     private lateinit var userManager: UserManager
     private lateinit var userInfos: List<UserInfo>
 
-    private var appBarLayout: AppBarLayout? = null
+    private val appBarLayout: AppBarLayout? = null
     private var searchText = ""
     private var customFilter: ((PackageInfo) -> Boolean)? = null
     private var comparator: ((PackageInfo, PackageInfo) -> Int)? = null
@@ -82,7 +82,6 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
         requireActivity().setTitle(getTitle())
-        appBarLayout = requireActivity().findViewById(R.id.app_bar)
         activityManager = requireContext().getSystemService(ActivityManager::class.java) as ActivityManager
         packageManager = requireContext().packageManager
         packageList = packageManager.getInstalledPackages(PackageManager.MATCH_ANY_USER)
@@ -99,10 +98,10 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         adapter = AppListAdapter()
-        recyclerView = view.findViewById<RecyclerView>(R.id.apps_list).also {
-            it!!.layoutManager = LinearLayoutManager(context)
-            it!!.adapter = adapter
-        } as RecyclerView
+        recyclerView = view.findViewById<RecyclerView>(R.id.apps_list)?.also {
+            it.layoutManager = LinearLayoutManager(context)
+            it.adapter = adapter
+        } ?: throw IllegalStateException("RecyclerView not found")
         refreshList()
     }
 
@@ -133,7 +132,7 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
         searchMenuItem.setOnActionExpandListener(object: MenuItem.OnActionExpandListener {
             override fun onMenuItemActionExpand(item: MenuItem): Boolean {
                 // To prevent a large space on tool bar.
-                appBarLayout!!.setExpanded(false /*expanded*/, false /*animate*/)
+                appBarLayout?.setExpanded(false /*expanded*/, false /*animate*/)
                 // To prevent user can expand the collapsing tool bar view.
                 ViewCompat.setNestedScrollingEnabled(recyclerView, false)
                 return true
@@ -141,7 +140,7 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
 
             override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
                 // We keep the collapsed status after user cancel the search function.
-                appBarLayout!!.setExpanded(false /*expanded*/, false /*animate*/)
+                appBarLayout?.setExpanded(false /*expanded*/, false /*animate*/)
                 ViewCompat.setNestedScrollingEnabled(recyclerView, true)
                 return true
             }
@@ -205,13 +204,13 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
             }
         }
         try {
-            activityManager.forceStopPackage(packageName)
+            activityManager.forceStopPackage(packageName);
         } catch (ignored: Exception) {
         }
     }
 
     private fun getKey(): String {
-        return "hide_developer_status"
+        return Settings.Secure.HIDE_DEVELOPER_STATUS
     }
 
     private fun refreshList() {
@@ -251,24 +250,24 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
             getLabel(packageInfo),
             packageInfo.applicationInfo!!.loadIcon(packageManager),
         )
-
+    
     private fun getLabel(packageInfo: PackageInfo) =
         packageInfo.applicationInfo!!.loadLabel(packageManager).toString()
-
+        
     private inner class AppListAdapter: ListAdapter<AppInfo, AppListViewHolder>(itemCallback) {
         private val selectedIndices = mutableSetOf<Int>()
         private var initialList = getInitialCheckedList().toMutableList()
-
+        
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             AppListViewHolder(layoutInflater.inflate(
                 R.layout.hide_developer_status_list_item, parent, false))
-
+            
         override fun onBindViewHolder(holder: AppListViewHolder, position: Int) {
             getItem(position).let {
                 holder.label!!.text = it.label
                 holder.packageName!!.text = it.packageName
                 holder.icon!!.setImageDrawable(it.icon)
-                holder.itemView!!.setOnClickListener {
+                holder.itemView.setOnClickListener {
                     if (selectedIndices.contains(position)) {
                         selectedIndices.remove(position)
                         onListUpdate(holder.packageName!!.text.toString(), false)
@@ -285,32 +284,32 @@ class HideDeveloperStatusSettings: Fragment(R.layout.hide_developer_status_layou
                 holder.checkBox!!.isChecked = selectedIndices.contains(position)
             }
         }
-
+        
         override fun submitList(list: List<AppInfo>?) {
             initialList = getInitialCheckedList().toMutableList()
             selectedIndices.clear()
             super.submitList(list)
         }
     }
-
+    
     private class AppListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val icon: ImageView? = itemView.findViewById(R.id.icon)
         val label: TextView? = itemView.findViewById(R.id.label)
         val packageName: TextView? = itemView.findViewById(R.id.packageName)
         val checkBox: CheckBox? = itemView.findViewById(R.id.checkBox)
     }
-
+    
     private data class AppInfo(
         val packageName: String,
         val label: String,
         val icon: Drawable,
     )
-
+    
     companion object {
         private val itemCallback = object: DiffUtil.ItemCallback<AppInfo>() {
             override fun areItemsTheSame(oldInfo: AppInfo, newInfo: AppInfo) =
                 oldInfo.packageName == newInfo.packageName
-
+            
             override fun areContentsTheSame(oldInfo: AppInfo, newInfo: AppInfo) =
                 oldInfo == newInfo
         }
